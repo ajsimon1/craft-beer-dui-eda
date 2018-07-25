2c0acd2e9dec503a5a301c33200545ee0324ca2d
Adam <adamjsimon1@yahoo.com>
2018-07-24 22:27:14 -0400

    intial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+## Exploratory Data Analysis Kaggle Craft Beer Dataset
+
+Found an interesting dataset on [craft beer and breweries](https://www.kaggle.com/nickhould/craft-cans).  Initially, I had basic
+questions on
+- What is the most popular beer style?
+- What state has the most breweries?
+- The highest avg ABV %?
+
+After these initial questions I decided to explore any relationships between
+fatalities from drunk driving as reported by another kaggle dataset on  [DUIs and fatalities](https://www.kaggle.com/bryanmaloney/dui-arrests-and-population-by-state-2015-usa/version/2) by state.
+
+Import necessary packages
+```python
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+```
+
+Begin by importing the csv into dataframes.  The craft beer set included 2
+csv files, 1 detailing actual beer and the other breweries.  The third csv is the
+DUI and fatalities set.
+
+```python
+# ################## load datasets #####################################
+df_beers = pd.read_csv('beers.csv', index_col=0)
+df_brews = pd.read_csv('breweries.csv', index_col=0)
+df_fatal = pd.read_csv('DUI.csv', index_col=0)
+```
+
+The beer datasets are combined into a single dataset based on the *brewery_id*
+column on the beer side acting as a foreign key to the an id column on the
+brewery side.  In order to ensure matching, an *id* column was set in the
+brewery dataset as **+1** of the loaded index.  The fatality data is left
+out due to avoid listing the summary statistics per state in duplicate for
+each beer style; these sets are explored in tandem later on.
+
+```python
+# add index as column for easy merging, adding to zero based index to
+# match 'brewery_id' column that is one base indexed
+df_brews['id'] = df_brews.index + 1
+```
+
+Once the *id/brewery_id* columns are in place, the sets are combined using
+and *outer* join.  I choose this join to retain non-matching rows in the
+final dataset.
+
+```python
+# merge datasets on 'brewery_id'/'id'
+df_comb = pd.merge(df_beers,
+                       df_brews,
+                       how='outer',
+                       left_on='brewery_id',
+                       right_on='id')
+```
+
+Cleaning continues:
+```python
+# ################### clean datasets ###################################
+# remove unamed column
+df_comb = df_comb.loc[:, ~df_comb.columns.str.contains('^Unnamed')]
+
+# rename columns
+col_rename = {'id_x':'beer_id', 'name_x':'beer_name', 'name_y':'brewery'}
+df_comb = df_comb.rename(index=str, columns=col_rename)
+
+# remove unecessary id_y column
+df_comb = df_comb.drop(labels='id_y', axis=1)
+```
---
+++ craft_beer.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 20:24:58 2018
+
+@author: Adam
+
+exploration into the craft beer dataset on kaggle
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ################## load datasets #####################################
+df_beers = pd.read_csv('beers.csv', index_col=0)
+df_brews = pd.read_csv('breweries.csv', index_col=0)
+df_fatal = pd.read_csv('DUI.csv', index_col=0)
+
+# ################### clean datasets ###################################
+# add index as column for easy merging, adding to zero based index to
+# match 'brewery_id' column that is one base indexed
+df_brews['id'] = df_brews.index + 1
+
+# merge datasets on 'brewery_id'/'id'
+df_comb = pd.merge(df_beers,
+                       df_brews,
+                       how='outer',
+                       left_on='brewery_id',
+                       right_on='id')
+
+# remove unamed column
+df_comb = df_comb.loc[:, ~df_comb.columns.str.contains('^Unnamed')]
+
+# rename columns
+col_rename = {'id_x':'beer_id', 'name_x':'beer_name', 'name_y':'brewery'}
+df_comb = df_comb.rename(index=str, columns=col_rename)
+
+# remove unecessary id_y column
+df_comb = df_comb.drop(labels='id_y', axis=1)
+
+# questions
+# avg abv per beer style
+df_comb.groupby(by='style')['abv'].mean()
+# total count per beer style
+df_comb.groupby(by='style')['abv'].count()
+# top 5 produced beer styles
+df_comb['style'].value_counts().head()
+# most common beer style in each state
+
+# most ounces per state?
+df_sum_ounces = df_comb.groupby('state')['ounces'].sum()
+df_sum_ounces.sort_values(ascending=False).head()
+# most popular style list
+df_comb['style'].count
+# top 5 states with most variety
+df_most_style_state = df_comb.groupby(by='state')['style'].count()
+df_most_style_state.sort_values(ascending=False).head()
+# top 5 most breweries per state
+df_brew_per_state = df_comb.groupby('state')['brewery'].count()
+df_brew_per_state.sort_values(ascending=False).head()
+# zoom in on NJ
+# get lats and longs for cities, plot
+# brewery with most styles
+df_style_brew_count = df_comb.groupby('brewery')['style'].count()
+df_style_brew_count.sort_values(ascending=False)
+# city with most breweries
+df_brew_per_city = df_comb.groupby('city')['brewery'].count()
+df_brew_per_city.sort_values(ascending=False).head()
+# most alcohol for all style, uselsess
+df_most_bitter_style = df_comb.groupby('style')['abv'].sum()
+df_most_bitter_style.sort_values(ascending=False).head()
+# avg alcohol per style
+df_avg_bitter_style = df_comb.groupby('style')['abv'].mean()
+df_avg_bitter_style.sort_values(ascending=False).head()
+# map of usa with breweries as volume per state
+# what brewery makes highest abv beer
+df_high_abv_style = df_comb.groupby('style')['abv'].max()
+df_high_abv_style.sort_values(ascending=False).head
+df_high_alc_style_state = df_comb.groupby(['state', 'beer_name'])['abv'].max()
+df_high_alc_style_state.sort_values(ascending=False)
+# count of style by state
+df_count_style_state = df_comb.groupby(['state', 'style'])['abv'].count()
+# TODO combine city state and get lat long for map
